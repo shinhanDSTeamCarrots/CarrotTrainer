1aeeb4b945d9fb6d8efc46d4c29e54c2f54c8b8e
mam675 <mam675@naver.com>
2023-11-30 02:19:18 +0900

    feat VO Update

---
+++ src/main/java/xyz/teamcarrot/myct/review/ReviewLikeVO.java
@@ -0,0 +1,10 @@
+package xyz.teamcarrot.myct.review;
+
+import lombok.Data;
+
+@Data
+public class ReviewLikeVO {
+	private int revie_like_no;
+	private int review_no;
+	private int member_no;
+}
---
+++ src/main/java/xyz/teamcarrot/myct/review/ReviewVO.java
@@ -0,0 +1,17 @@
+package xyz.teamcarrot.myct.review;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class ReviewVO {
+	private int review_no;
+	private String content;
+	private int point;
+	private Timestamp regist_date;
+	private int like_cnt;
+	private int goods_no;
+	private int member_no;
+	private String member_name;
+}
